15ff2ca24073786835557771bd0cfb7b2d53cb32
ganytbs0910 <nakakou2002@icloud.com>
2024-12-26 22:53:15 +0900

    本番環境だけ本物の広告を表示するように

--- src/config/AdConfig.ts
+++ src/config/AdConfig.ts
@@ -1,6 +1,18 @@
+import { TestIds } from 'react-native-google-mobile-ads';
+
+const isDevelopment = __DEV__;
+
 export const AD_CONFIG = {
-  IOS_APP_ID: 'ca-app-pub-4317478239934902~6966717306',
-  IOS_INTERSTITIAL_ID: 'ca-app-pub-4317478239934902/8782634924',
-  IOS_BANNER_ID: 'ca-app-pub-4317478239934902/5594861664',
-  IOS_REWARDED_ID: 'ca-app-pub-4317478239934902/4496232521',
-};
+  IOS_APP_ID: isDevelopment 
+    ? TestIds.APP 
+    : 'ca-app-pub-4317478239934902~6966717306',
+  IOS_INTERSTITIAL_ID: isDevelopment 
+    ? TestIds.INTERSTITIAL 
+    : 'ca-app-pub-4317478239934902/8782634924',
+  IOS_BANNER_ID: isDevelopment 
+    ? TestIds.BANNER 
+    : 'ca-app-pub-4317478239934902/5594861664',
+  IOS_REWARDED_ID: isDevelopment 
+    ? TestIds.REWARDED 
+    : 'ca-app-pub-4317478239934902/4496232521',
+} as const;
